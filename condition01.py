51a2ec0eaed57c134e4e9286dcdba5ae3f5d4c03
Saqib Shah <saqibshah@live.com>
2024-06-15 10:20:17 +0500

    Start working with conditions

---
+++ condition01.py
@@ -0,0 +1,12 @@
+#print(10>1)
+#print("cat" == "dog")
+#print(1 != 2)
+
+# print(1 < "1")
+# print(1 == "1")
+
+print("yellow" > "cyan" and "brown" > "magenta")
+
+print(25 > 50 or 1 != 2)
+
+print(32 == 30 + 2 )
