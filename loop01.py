42eda24dcdb2556222d7e547dcbe9a54a0ee8e3c
Saqib Shah <saqibshah@live.com>
2024-06-27 10:42:25 +0500

    Working with while loop

--- loop01.py
+++ loop01.py
@@ -3,3 +3,24 @@ while x < 5:
     print("Not there yet", x)
     x = x + 1
 print("x " + str(x))
+
+
+def attempts(n):
+    x = 1
+    while x <= n:
+        print("Attempt " + str(x))
+        x +=1
+    print("Done")
+
+attempts(5) 
+
+def get_username(username):
+    return username
+
+# error code
+# username = get_username("xaqib")
+
+# while not valid_username(username):
+#     print("Invalid username")
+#     username = get_username()
+
---
+++ while_loop.py
@@ -0,0 +1,5 @@
+my_variable = 2
+
+while my_variable < 10:
+    print(my_variable, "Hello")
+    my_variable += 1
