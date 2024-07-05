655036dcdcb7a93f7e92b005e3468d994ed863ab
Saqib Shah <saqibshah@live.com>
2024-07-05 16:22:27 +0500

    Work with loops and nested loop

--- loop01.py
+++ loop01.py
@@ -24,3 +24,38 @@ def get_username(username):
 #     print("Invalid username")
 #     username = get_username()
 
+
+
+for x in range(5):
+    print(x)
+
+friends = ['Nadir', 'Iname', 'Shahrukh', 'Danish']
+for friend in friends:
+    print("Hello " + friend)
+
+
+values = [24,53, 68,99,35]
+sum = 0
+length = 0
+
+for value in values:
+    sum += value
+    length += 1
+print("Total sum: " + str(sum) + " Average: " + str(sum/length))
+
+
+
+for n in range(1,5,6):
+    print(n)
+
+
+for n in range (0,11,2):
+    print(n)
+
+
+for n in range (2, 7+1):
+    print(n *3)
+
+for n in range (2, -2, -1):
+    print(n)
+    
---
+++ nestedloop.py
@@ -0,0 +1,9 @@
+for left in range(7):
+    for right in range(left, 7):
+        print("["+ str(left)+ "|" + str(right)+ "]", end=" " )
+    print() 
+
+# for left in range(7):
+#   for right in range(left, 7):
+#     print("[" + str(left) + "|" + str(right) + "]", end=" ")
+#   print()
